9e8b9bba9c1aeef69ae75a182126442417bd7147
VanMatsuda <165146810+FelipeCatarino@users.noreply.github.com>
2024-08-28 12:27:16 -0300

    [fix] Primeiro erro.
    Co-Authored-By: 7marmath <162700948+7marmath@users.noreply.github.com>
    Co-Authored-By: Felipe Catarino <94021130+felipaodaCg@users.noreply.github.com>
    Co-Authored-By: Shausinaguer <149312021+shausinaguer@users.noreply.github.com>

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -4,6 +4,8 @@ import entities.Pessoa;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        System.out.println("TITLE TO DEVELOPMENT ");
+
         Pessoa ps = new Pessoa("Felipe", 25);
 
         System.out.println("Hello, World! " + ps);
