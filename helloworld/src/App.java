cd30fcd75c29854da3e5d289dddaafd851acad55
VanMatsuda <vanmatsuda@gmail.com>
2024-08-28 11:55:13 -0300

    [feat] inclusão Classe Pessoa
    [feat] Foi-se colocado a Classe Pessoa junto com os métodos de acesso.
    
    Co-Authored-By: 7marmath <162700948+7marmath@users.noreply.github.com>
    Co-Authored-By: Felipe Catarino <94021130+felipaodaCg@users.noreply.github.com>
    Co-Authored-By: Shausinaguer <149312021+shausinaguer@users.noreply.github.com>

---
+++ helloworld/src/App.java
@@ -0,0 +1,11 @@
+
+import entities.Pessoa;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Pessoa ps = new Pessoa("Felipe", 25);
+
+        System.out.println("Hello, World! " + ps);
+    }
+}
